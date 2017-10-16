4035d7d4f1156729427ba5a80aa68836f88f3527
Anurag Vempati <vanurag9@gmail.com>
2017-10-16 15:17:33 +0200

    minor

--- include/rovio/RovioNode.hpp
+++ include/rovio/RovioNode.hpp
@@ -226,12 +226,12 @@ class RovioNode{
 //      subImu_ = nh_.subscribe("/firefly/vi_sensor/imu", 1000, &RovioNode::imuCallback,this);
 //      subImg0_ = nh_.subscribe("/firefly/vi_sensor/left/image_raw", 1000, &RovioNode::imgCallback0,this);
 //      subImg1_ = nh_.subscribe("/firefly/vi_sensor/right/image_raw", 1000, &RovioNode::imgCallback1,this);
-      subImu_ = nh_.subscribe("/duo3d/imu/data_raw", 50, &RovioNode::imuCallback,this);
-      subImg0_ = nh_.subscribe("/duo3d/left/image_raw", 10, &RovioNode::imgCallback0,this);
-      subImg1_ = nh_.subscribe("/duo3d/right/image_raw", 10, &RovioNode::imgCallback1,this);
-//      subImu_ = nh_.subscribe("imu0", 1000, &RovioNode::imuCallback,this);
-//      subImg0_ = nh_.subscribe("cam0/image_raw", 1000, &RovioNode::imgCallback0,this);
-//      subImg1_ = nh_.subscribe("cam1/image_raw", 1000, &RovioNode::imgCallback1,this);
+//      subImu_ = nh_.subscribe("/duo3d/imu/data_raw", 50, &RovioNode::imuCallback,this);
+//      subImg0_ = nh_.subscribe("/duo3d/left/image_raw", 10, &RovioNode::imgCallback0,this);
+//      subImg1_ = nh_.subscribe("/duo3d/right/image_raw", 10, &RovioNode::imgCallback1,this);
+        subImu_ = nh_.subscribe("imu0", 50, &RovioNode::imuCallback,this);
+        subImg0_ = nh_.subscribe("cam0/image_raw", 10, &RovioNode::imgCallback0,this);
+        subImg1_ = nh_.subscribe("cam1/image_raw", 10, &RovioNode::imgCallback1,this);
     }
       // for aligning with groundtruth (mocap). SET noFeedbackToRovio = TRUE
       subGroundtruth_ = nh_.subscribe("drz_rig/raw_transform", 1000, &RovioNode::groundtruthCallback,this);
